327e933c2bcef1c2166590da4ae1fb3d06734cc3
NicoleBorbe <nicole.borbe@gmail.com>
2022-08-28 22:18:34 +0200

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let number = document.getElementById("number");
+
+function double() {
+    document.getElementById("number").value * 2;
+    console.log(number.value * 2);
+    ergebnis.innerHTML = (number.value * 2);
+}
+
